dcc3423be419be6f22bebcb5c80c551869ba48e6
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-03 22:08:25 +0800

    Added blog ref to schema

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose';
 
 const MessageSchema = new Schema({
+    blog: { type: Schema.Types.ObjectId, ref: 'User' },
     timestamp: Date,
     text: String,
     author: { type: Schema.Types.ObjectId, ref: 'User' },
