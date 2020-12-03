03301d651e4e3ab6cf19c7b35a129ebb52c3bdf9
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-03 22:22:58 +0800

    added blog routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { blogController } from '../controllers';
 // Init router and path
 const router = Router();
 
@@ -7,6 +7,9 @@ const router = Router();
 router.use('/users', (req, res, next) => {
     res.json('U');
 });
+
+router.use('/blogs', blogController.blogs_get);
+router.post('/blogs', blogController.blog_post);
 
 // Export the base-router
 export default router;
