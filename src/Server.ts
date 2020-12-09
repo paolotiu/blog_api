39d1d2127359bf53169fda97af3f3de3f05eeea2
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-09 14:37:58 +0800

    added cors

--- src/Server.ts
+++ src/Server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
@@ -9,7 +10,7 @@ import 'express-async-errors';
 
 import BaseRouter from './routes';
 import logger from './shared/Logger';
-
+import cors from 'cors';
 const app = express();
 const { BAD_REQUEST } = StatusCodes;
 
@@ -35,11 +36,16 @@ db.once('open', function () {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.use('/test', (req, res, next) => {
+    res.send('cors');
+});
 
 // Security
 if (process.env.NODE_ENV === 'production') {
@@ -52,6 +58,7 @@ app.use('/', BaseRouter);
 // Print API errors
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log('hey');
     logger.err(err, true);
     return res.status(BAD_REQUEST).json({
         error: err.message,
