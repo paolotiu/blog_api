91fcd62aa7f396f934f8d355399088a2f8f17c3e
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-12 16:24:43 +0800

    mongoose setup

--- src/Server.ts
+++ src/Server.ts
@@ -22,6 +22,7 @@ import mongoose from 'mongoose';
 mongoose.connect(process.env.MONGODB_URI as string, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
 });
 const db = mongoose.connection;
 // eslint-disable-next-line no-console
